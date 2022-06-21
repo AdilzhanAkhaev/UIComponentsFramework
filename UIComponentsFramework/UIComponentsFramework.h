1fdbb0723033231cd9adb38c523e81bf6e2dc2fc
Adilzhan.Akhayev <a.akhayev@aviata.me>
2022-06-21 07:33:32 +0600

    Initial Commit

---
+++ UIComponentsFramework/UIComponentsFramework.h
@@ -0,0 +1,18 @@
+//
+//  UIComponentsFramework.h
+//  UIComponentsFramework
+//
+//  Created by Aviata on 21.06.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIComponentsFramework.
+FOUNDATION_EXPORT double UIComponentsFrameworkVersionNumber;
+
+//! Project version string for UIComponentsFramework.
+FOUNDATION_EXPORT const unsigned char UIComponentsFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIComponentsFramework/PublicHeader.h>
+
+
